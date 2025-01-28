9160108b357f134d5a7e91d2238cc2b2d5adcd95
tucecifci <tucecifcii@gmail.com>
2025-01-28 16:20:35 +0300

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <img
+          style={{ width: "60px" }}
+          src="/src/assets/bird-logo.png"
+          alt="logo"
+        />
+        <p>Pick&Pay</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
