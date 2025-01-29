4752b5e1bf6c28e25b644deb1e408092c538c3cf
tucecifci <tucecifcii@gmail.com>
2025-01-29 12:53:32 +0300

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,17 +20,7 @@ function Header() {
   const changeTheme = () =>{
     setTheme(prevTheme => !prevTheme);
   }
-  // const changeTheme = () =>{
-  //     const root = document.getElementById("root");
-  //     if(theme){
-  //         root.style.backgroundColor = "black";
-  //         root.style.color = "#fff";
-  //     }else{
-  //         root.style.backgroundColor = "#fff";
-  //         root.style.color = "black";
-  //     }
-  //     setTheme(!theme);
-  // }
+ 
   return (
     <div className="flex items-center justify-between mt-40">
       {/* Logo ve Başlık */}
@@ -39,7 +29,6 @@ function Header() {
         <h1 className="font-black ml-2">Pick&Pay</h1>
       </div>
 
-      {/* Arama ve Sepet Grubu */}
       <div className="flex items-center border-b-2 border-gray-400">
         <input
           type="text"
@@ -49,16 +38,15 @@ function Header() {
         <CiShoppingCart className="text-2xl text-gray-500 cursor-pointer ml-2" />
       </div>
 
-      {/* Güneş ve Ay İkonları */}
       <div className="flex items-center space-x-4">
         {theme ? (
           <MdOutlineWbSunny
             onClick={changeTheme}
-            className="text-xl text-gray-500 cursor-pointer"
+            className="text-xl text-yellow-500 cursor-pointer"
           />
         ) : (
           <IoMdMoon
-            className="text-xl text-gray-500 cursor-pointer"
+            className="text-xl text-gray-800 cursor-pointer"
             onClick={changeTheme}
           />
         )}
