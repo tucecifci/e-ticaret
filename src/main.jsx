7fd58a176f0b3a486b1dda5af7b4a34d46ab7017
tucecifci <tucecifcii@gmail.com>
2025-01-30 14:49:51 +0300

    products added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react'; // React'i açıkça import edin
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.jsx';
+import React from "react"; // React'i açıkça import edin
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
- 
 );
