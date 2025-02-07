f615bee01dff2fbfc6416df6320353545b176459
tucecifci <tucecifcii@gmail.com>
2025-02-07 16:33:33 +0300

    detail page added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Product({ product }) {
   const { id, price, image, title, description } = product;
-  console.log(product);
+  const navigate = useNavigate();
+ 
   return (
       <div className="!p-2 rounded-lg w-[200px] h-[380px] cursor-pointer shadow-lg hover:shadow-gray-400 hover:shadow-lg transform transition-all duration-500 ">
         <img src={image} alt="img" className="w-[180px] h-[240px]" />
@@ -11,7 +13,7 @@ function Product({ product }) {
           <h3 className="font-bold">{price} TL </h3>
         </div>
         <div className="flex justify-center">
-            <button className="bg-gray-200 !px-8 !py-2 !mt-4 rounded-md cursor-pointer hover:font-bold ">Detail Page</button>
+            <button onClick={() => navigate("./product-details/" + id)} className="bg-gray-200 !px-8 !py-2 !mt-4 rounded-md cursor-pointer hover:font-bold">Detail Page</button>
         </div>
       </div>
   );
